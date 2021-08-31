251ea5a46934c2ccae6af831833ab0d40229394e
keegtrevill <keegantrevillian@gmail.com>
2021-08-30 20:27:45 -0500

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+// HINT: You can delete this console.log after you no longer need it! DONE
+
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Keegan Trevillian" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
@@ -13,11 +13,47 @@ let sugar = 0   // Sugar Sprinkle
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Ginger Bread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
+    
 
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
-})
+{
+    document.getElementById('add-gb').addEventListener('click', function () {
+        let addGb =  gb += 1
+        document.getElementById('qty-gb').textContent = addGb
+    
+    })
 
-// TODO: Hook up event listeners for the rest of the buttons
+    document.getElementById('minus-gb').addEventListener('click', function () {
+        let minusGb =  gb -= 1
+        document.getElementById('qty-gb').textContent = minusGb  
+    })    
+
+
+    document.getElementById('add-cc').addEventListener('click', function () {
+        let addCc =  cc += 1
+        document.getElementById('qty-cc').textContent = addCc
+    
+    })
+
+    document.getElementById('minus-cc').addEventListener('click', function () {
+        let minusCc =  cc -= 1
+        document.getElementById('qty-cc').textContent = minusCc 
+    })    
+
+    document.getElementById('add-sugar').addEventListener('click', function () {
+        let addSugar =  sugar += 1
+        document.getElementById('qty-sugar').textContent = addSugar
+    
+    })
+
+    document.getElementById('minus-sugar').addEventListener('click', function () {
+        let minusSugar =  sugar -= 1
+        document.getElementById('qty-sugar').textContent = minusSugar 
+    })
+    
+    let totalCookies = 
+    totalCookies = gb + cc + sugar
+    document.getElementById('qty-total').textContent = totalCookies
+
+}
+
+
